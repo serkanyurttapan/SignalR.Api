cfee90c43bd58ce90e57d29ea23153fe963a5101
serkanyurttapan_ <serkan.yurttapan@asistbt.com.tr>
2020-08-30 17:41:51 +0300

    Add project files.

---
+++ SignalR.Api/Hubs/ConnectionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.Api.Hubs
+{
+    public class ConnectionHub : Hub
+    {
+        public async Task SendName(string name)
+        {
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+    }
+}
